37f42023b0839aebd20a74a30139996735d87558
edjunma <junwenma@buffalo.edu>
2019-06-22 23:38:53 -0400

    adding play function and event listeners

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+
+const scoreboard = {
+	player: 0,
+	computer: 0
+};
+
+// Play game
+function play(e) {
+	restart.style.display = 'inline-block';
+	const playerChoice = e.target.id;
+	const computerChoice = getComputerChoice();
+}
+
+// Event listeners
+choices.forEach(choice => choice.addEventListener('click', play));
